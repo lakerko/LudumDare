a423e00d6de03b12d88d4ed13e559bc57dde726c
lakerko <martin.ockovsky@gmail.com>
2014-04-28 21:29:35 +0200

    Basic version

--- Player.js
+++ Player.js
@@ -1,6 +1,8 @@
 function Player() {
     this.x = 100;
     this.y = 250;
+    this.width = 50;
+    this.height = 50;
     this.velocity = 0;
     this.acceleration = 3;
 }
@@ -14,10 +16,10 @@ Player.prototype.reset = function() {
 Player.prototype.update = function(delta) {
     if (keysDown[32] && !jump) {
         jump = true;
-        this.velocity = 1;
-        this.y -= 1;
+        this.velocity = 5;
+        this.y -= 2;
     }
-    this.velocity -= this.acceleration * (delta / 1000);
+    this.velocity -= this.acceleration * (delta / 500);
 
 
     this.y -= this.velocity;
@@ -30,5 +32,18 @@ Player.prototype.update = function(delta) {
 
 Player.prototype.render = function() {
     ctx.fillStyle = "rgb(200, 200, 200)";
-    ctx.fillRect(this.x, this.y, 100 , 50);
+    ctx.fillRect(this.x, this.y, 50 , 50);
+}
+
+Player.prototype.collisionDetection = function() {
+    for (var i = 0; i < aTubes.length; i++) {
+        if (aTubes[i].x < canvas.width) {
+            if (this.x <= aTubes[i].x + aTubes[i].width &&
+                aTubes[i].x <= this.x + this.width &&
+                this.y <= aTubes[i].y + aTubes[i].height &&
+                aTubes[i].y <= this.y + this.height) {
+                reset();
+            }
+        }
+    }
 }
--- Tube.js
+++ Tube.js
@@ -2,35 +2,41 @@ function Tube(x, y, width) {
     this.x = x;
     this.y = y;
     this.width = width;
+    this.height = 300;
 }
-var aTubes = [];
+
 var aBuffer = [];
 
 function spawnTube() {
     aTubes = [];
-    for (var i = 0; i < 19; i += 2) {
-
-            aTubes[i] = new Tube(canvas.width + i * 200, 0, 100);
-            aTubes[i + 1] = new Tube(canvas.width + i * 200, canvas.height - 200, 100);
-
-
+    var randomX;
+    var randomY;
+    for (var i = 0; i < 7; i += 2) {
+        randomX = randomTubeX();
+        randomY = randomTubeY();
+        if (i == 0) {
+            aTubes[i] = new Tube(canvas.width + 10, randomY, 100);
+            aTubes[i + 1] = new Tube(canvas.width + 10, randomY + 300 + 150, 100);
+        } else {
+            aTubes[i] = new Tube(aTubes[i - 1].x + randomX, randomY, 100);
+            aTubes[i + 1] = new Tube(aTubes[i - 1].x  + randomX, randomY + 300 + 150, 100);
+        }
     }
 }
 
 function updateTube(delta) {
-    timeCheck += delta;
-    if (timeCheck >= 10) {
-        timeCheck = 0;
-        for (var i = 0; i < aTubes.length; i++) {
-            aTubes[i].x -= 3;
+    for (var i = 0; i < aTubes.length; i++) {
+        aTubes[i].x -= 200 * (delta / 1000);
+        if ( aTubes[i].x < canvas. width) {
             if (aTubes[i].x + aTubes[i].width <= 0) {
+                var randomX = randomTubeX();
+                var randomY = randomTubeY();
                 aBuffer = [];
                 aBuffer = aTubes.slice(0, 2);
                 aTubes.splice(0, 2);
-                aBuffer[0].x = aTubes[aTubes.length - 2].x + 400;
-                aBuffer[1].x = aTubes[aTubes.length - 2].x + 400;
+                aBuffer[0].x = aTubes[aTubes.length - 1].x + randomX;
+                aBuffer[1].x = aTubes[aTubes.length - 1].x + randomX;
                 aTubes.splice(aTubes.length, 0, aBuffer[0], aBuffer[1]);
-
             }
         }
     }
@@ -38,7 +44,27 @@ function updateTube(delta) {
 
 function renderTube() {
     for (var i = 0; i < aTubes.length; i++){
-        ctx.fillStyle = "rgb(50, 50, 50)";
-        ctx.fillRect(aTubes[i].x, aTubes[i].y, 100 , 200);
+        if (aTubes[i].x < canvas.width) {
+            if (i % 2 == 0){
+                ctx.fillStyle = "rgb(250, 50, 50)";
+                //ctx.fillRect(aTubes[i].x, 0, 100 , aTubes[i].y + 300);
+                ctx.strokeRect(aTubes[i].x, 0, 100 , aTubes[i].y + 300);
+            } else {
+                ctx.fillStyle = "rgb(250, 50, 50)";
+                //ctx.fillRect(aTubes[i].x, aTubes[i].y, 100 , canvas.width);
+                ctx.strokeRect(aTubes[i].x, aTubes[i].y, 100 , canvas.width);
+            }
+
+        }
     }
+}
+
+function randomTubeX() {
+    var maximum = 5;
+    var minimum = 3;
+    return 100 * Math.floor(Math.random()* (maximum - minimum + 1) + minimum);
+}
+
+function randomTubeY() {
+    return - Math.floor(Math.random()* 250);
 }
